5d7b97e194b0307cadfe840c9876300f795fd5ec
Mar-Issah <masy370@gmail.com>
2023-10-06 17:50:44 -0400

    add new 2-hbnb.html file

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,4 +18,13 @@ $('document').ready(function () {
       $('.amenities h4').text(Object.values(amenities).join(', '));
     }
   });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    // console.log(data);
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
